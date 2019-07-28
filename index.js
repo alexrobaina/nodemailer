2da34e96905aae342e671411a24439cd691d3029
Alex Robaina <alexrobaina@Alexs-MacBook-Pro.local>
2019-07-28 17:58:54 -0300

    google11

--- index.js
+++ index.js
@@ -30,22 +30,26 @@ app.use((req, res, next) => {
 
 app.post('/api/form', (req, res) => {
 
+    console.log(req.body)
         let transporter = nodemailer.createTransport({
-            // service: 'gmail',
-            // type: 'service_account',
-            // clientId: '361223958674-qkls4qokro669qrv8nh4sfs8ch75ai6a.apps.googleusercontent.com',
-            // clientSecret: 't_84TsL4cxE7CO7AfGasuxM3',
             host: 'smtp.gmail.com',
             port: 465,
             secure: true,
             auth: {
-                // user: process.env.EMAIL,
-                // pass: process.env.PASSWORD
                 type: 'OAuth2',
                 user: 'info.guacamayos@gmail.com',
-                refreshToken: '1/HrapCZx5UTOny59wacUh9q5xNEMqvRrnukahB8OsWi6CoYX9vMiG45Fq2dXSxun9',
+                // refreshToken: '1/HrapCZx5UTOny59wacUh9q5xNEMqvRrnukahB8OsWi6CoYX9vMiG45Fq2dXSxun9',
                 accessToken: 'ya29.GltTB1J-kE0Nhicql4Y2dLag9Hpqa7cChZILGZ7g0ZoorNiJUkZ7TzBitSGEJML2c9BmHKiYHXuWBkzeA6seKieB0A3wz4JW4GiUTRrWoIYMoDQ9WJJO8sYZqHw-',
-                expires: 3600,
+                // expires: 3600,
+            }
+        });
+
+        transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
+            let accessToken = userTokens[user];
+            if(!accessToken){
+                return callback(new Error('Unknown user'));
+            }else{
+                return callback(null, accessToken);
             }
         });
 
@@ -60,7 +64,7 @@ app.post('/api/form', (req, res) => {
         transporter.sendMail(mailOptions, function(err, data) {
             if (err) {
                 console.log('Error Occurs');
-                res.send('error hola mundo');
+                res.send('error hola mundo 3');
             } else {
                 console.log('Email sent!!!!');
                 res.send('ok');
