d9a07f171e618b35f3355cdbd7f13549a03e2f89
Alex Robaina <alexrobaina@Alexs-MacBook-Pro.local>
2019-07-29 23:23:28 -0300

    testing mensajes email 27

--- index.js
+++ index.js
@@ -36,14 +36,13 @@ app.post('/api/form', (req, res) => {
             port: 465,
             secure: true,
             auth: {
-                // type: 'OAuth2',
-                // user: 'info.guacamayos@gmail.com',
-                // clientId: '361223958674-3d07mbqfuhf44tp0b550244mtb8ctr3n.apps.googleusercontent.com',
-                // clientSecret: 'JtThfUvmsr2cfA3Qpch_j4gg',
-                // refreshToken: '1/fSiZS0RVCgxGV6Ct86w0KoOiIPmsdlnzXOOvB-8C6m8',
+                type: 'OAuth2',
+                user: 'info.guacamayos@gmail.com',
+                clientId: '361223958674-u33j40r18i6nh3ajdje7ao4epoup5buo.apps.googleusercontent.com',
+                clientSecret: 'jqyFW5tUqlZWgIMGYOtl-iB4',
+                refreshToken: '1/fSiZS0RVCgxGV6Ct86w0KoOiIPmsdlnzXOOvB-8C6m8',
                 accessToken: 'ya29.GltUB8ThaFt7RAeMJDFgrZckvFFlEPMo6kvDiGqmFbhGxxmLO49Yk47V-8FCShPZjetHpfJnzInuhQyNGIWJIAfZn_hOvDHMtlQzn8W2bhLujpEQtWBs7GWqppGL',
-                // expires: 3600
-
+                expires: 3600
             }
         });
 
@@ -52,7 +51,7 @@ app.post('/api/form', (req, res) => {
             to: 'lalimiramon@gmail.com',
             bcc: 'info.guacamayos@gmail.com',
             subject: 'Testing',
-            text: `LO LOGREEEEEEEEEE mi amoooooooooor TE AMOOOOOOOO`
+            text: `Mensaje testing`
         }
         
         transporter.sendMail(mailOptions, function(err, data) {
